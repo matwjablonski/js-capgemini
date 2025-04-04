144f4eba8b4bfded93dd3867f38673c2c91f224d
Mateusz Jabłoński <mail@mateuszjablonski.com>
2025-04-04 15:35:29 +0200

    update

--- projects/final/cypress/e2e/books.cy.js
+++ projects/final/cypress/e2e/books.cy.js
@@ -12,4 +12,39 @@ describe('Books App', () => {
     cy.get('.book-item').should('have.length.greaterThan', 0);
   });
 
+  it('should add a new book', () => {
+    cy.get('input[name="title"]').type('Nowa książka');
+    cy.get('input[name="author"]').type('Nowy autor');
+    cy.get('input[name="category"]').type('Nowa kategoria');
+    cy.get('.book-form button[type="submit"]').click();
+
+    cy.get('.books-list').should('contain', 'Nowa książka');
+  });
+
+  it('should toggle favorite status', () => {
+    cy
+      .get('.book-item')
+      .first()
+      .find('.action-button')
+      .should('contain', 'Dodaj do ulubionych')
+      .click();
+    
+    cy
+      .get('.book-item')
+      .first()
+      .should('have.class', 'favorite');
+
+    cy
+      .get('.book-item')
+      .first()
+      .find('.action-button')
+      .should('contain', 'Usuń z ulubionych')
+      .click();
+
+    cy
+      .get('.book-item')
+      .first()
+      .should('not.have.class', 'favorite');
+  });
+
 });
