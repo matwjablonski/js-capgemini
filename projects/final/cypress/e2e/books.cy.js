bd6623796a00b1dd1d24757ffc617171bfe8abba
Mateusz Jabłoński <mail@mateuszjablonski.com>
2025-04-04 15:23:50 +0200

    task 19

--- projects/final/cypress/e2e/books.cy.js
+++ projects/final/cypress/e2e/books.cy.js
@@ -3,5 +3,13 @@ describe('Books App', () => {
     cy.visit('http://localhost:3000');
   });
 
-  
+  it('should display the correct title', () => {
+    cy.get('h1').should('contain', 'Witaj w naszej aplikacji książkowej');
+  });
+
+  it('should display a list of books', () => {
+    cy.get('.books-list').should('exist');
+    cy.get('.book-item').should('have.length.greaterThan', 0);
+  });
+
 });
---
+++ tasks/task-19.md
@@ -0,0 +1,3 @@
+## Zadanie nr 19
+
+- w katalogu `cypress/e2e/` utwórz plik `books.cy.js` i przygotuj test, który przejdzie przez całe flow dodawnia nowych książek, oznaczenia ich jako ulubione i usuwania z ulubionych
